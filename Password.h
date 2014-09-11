bf0e7eca07b53c8708157b8584a4833f0b446183
Kevin Brown <kabrown44@students.tntech.edu>
2014-09-11 15:36:23 -0400

    Upload 1

--- Password.h
+++ Password.h
@@ -1,7 +1,9 @@
 #if !defined PASSWORD_H
 #define PASSWORD_H
-
-//complete the includes
+#include "ListArray.h"
+using CSC2110::ListArray;
+#include "Text.h"
+using CSC2110::String;
 
 class Password
 {
@@ -14,6 +16,14 @@ class Password
       int getNumMatches(String* curr_word, String* word_guess);
 
    public:
+	Password(); //constructor
+	~Password(); //destructor (there is work to do here, delete the individual words)
+	void addWord(String* word); //add a word to the list of possible passwords
+	void guess(int try_password, int num_matches); //index of guessed word in the list of all words (1-based), number of matches reported by fallout 3, update viable passwords list
+	int getNumberOfPasswordsLeft(); //returns the number of possible passwords remaining
+	void displayViableWords(); //display the current list of possible passwords
+	int bestGuess(); //the best word in the original list to guess next (done for you)
+	String* getOriginalWord(int index); //get a word from the original list of all passwords, 1-based
 
 
 };
